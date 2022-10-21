58bf9fda58320843823257615958976e26a67931
Yuko <yyamano@my.centennialcollege.ca>
2022-10-20 21:34:14 -0400

    bootstrap and jquery added

--- server/server.js
+++ server/server.js
@@ -8,13 +8,20 @@ const PORT = 3000;
 //app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+//console.log(__dirname);  //server
+//console.log(__filename); //server.js
 
+//We need to write this code to use bootstrap
+//app.use(express.static(path.join(__dirname, '../node_modules/bootstrap/dist')));
+  
 //requireでuser.jsを読み込む
 const userRouter = require("../router/user");
 
 
 //useメソッドでユーザー関連のルーティングを設定
 app.use("/",userRouter);
+
+
 
 
 app.listen(PORT,()=>{
